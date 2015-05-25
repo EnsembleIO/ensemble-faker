e9ded4d3cd5efc9e925646489f3d9deb16adc501
robbee <alexandre.bonte@beesearch.fr>
2015-05-25 22:25:06 +0200

    .

--- create-all.sh
+++ create-all.sh
@@ -2,9 +2,40 @@ echo "########################################"
 echo " CREATE SAMPLE DATA  "
 echo "########################################"
 
-echo "### Localhost ###"
+echo "### Localhost : Delete Index ###"
+curl -XDELETE 'http://localhost:9200/hackathon'
+
+echo "--> PRESS ANY KEY..."
+read touche
+
+echo "### Localhost : Create Index ###"
+curl -XPUT 'http://localhost:9200/hackathon'
+
+echo "--> PRESS ANY KEY..."
+read touche
+
+echo "### Localhost : Insert news ###"
 cat sample-news.json | jq -c '.[] | {"index": {"_index": "hackathon", "_type": "news"}}, .' | curl -XPOST http://localhost:9200/_bulk --data-binary @-
 
-echo "### Bonsai ###"
+echo "--> PRESS ANY KEY..."
+read touche
+
+echo "### Bonsai : Delete Index ###"
+curl -XDELETE 'https://fzan4xer:1l0nx4qxyuup12ul@azalea-9794680.us-east-1.bonsai.io/hackathon'
+
+echo "--> PRESS ANY KEY..."
+read touche
+
+echo "### Bonsai : Create Index ###"
+curl -XPUT 'https://fzan4xer:1l0nx4qxyuup12ul@azalea-9794680.us-east-1.bonsai.io/hackathon'
+
+echo "--> PRESS ANY KEY..."
+read touche
+
+echo "### Bonsai : Insert news ###"
 cat sample-news.json | jq -c '.[] | {"index": {"_index": "hackathon", "_type": "news"}}, .' | curl -XPOST https://fzan4xer:1l0nx4qxyuup12ul@azalea-9794680.us-east-1.bonsai.io/_bulk --data-binary @-
+
+echo "--> PRESS ANY KEY..."
+read touche
+
 
