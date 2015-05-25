175ae9c54b3ebbd948f3acdf303c304b55d615bf
robbee <alexandre.bonte@beesearch.fr>
2015-05-25 12:14:15 +0200

    add noemie's story board news

--- create-all.sh
+++ create-all.sh
@@ -2,5 +2,9 @@ echo "########################################"
 echo " CREATE SAMPLE DATA  "
 echo "########################################"
 
-cat news-sample.json | jq -c '.[] | {"index": {"_index": "hackathon", "_type": "news"}}, .' | curl -XPOST https://fzan4xer:1l0nx4qxyuup12ul@azalea-9794680.us-east-1.bonsai.io/_bulk --data-binary @-
+echo "### Localhost ###"
+cat sample-news.json | jq -c '.[] | {"index": {"_index": "hackathon", "_type": "news"}}, .' | curl -XPOST http://localhost:9200/_bulk --data-binary @-
+
+echo "### Bonsai ###"
+cat sample-news.json | jq -c '.[] | {"index": {"_index": "hackathon", "_type": "news"}}, .' | curl -XPOST https://fzan4xer:1l0nx4qxyuup12ul@azalea-9794680.us-east-1.bonsai.io/_bulk --data-binary @-
 
